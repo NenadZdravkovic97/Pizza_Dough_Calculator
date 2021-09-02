eb761e47510a1b7fa196831a34f9b6813ae7fbd9
NenadZdravkovic97 <nenadzdravkovic97@yahoo.com>
2021-09-02 19:40:17 +0200

    Added sticky nav and fixed imgs

--- js/newApp.js
+++ js/newApp.js
@@ -1,9 +1,28 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.main-nav-list');
 
-hamburger.addEventListener('click', mobileMenu);
-
-function mobileMenu() {
+// Sticku nav
+const header = document.querySelector('.header');
+const nav = document.querySelector('.header-nav');
+hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
-}
+});
+
+// Sticky nav
+const navHeight = nav.offsetHeight;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+    const [entry] = entries;
+    console.log(entry);
+    if (!entry.isIntersecting) nav.classList.add('sticky');
+    else nav.classList.remove('sticky');
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
